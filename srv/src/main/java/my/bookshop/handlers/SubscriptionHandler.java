0c7c0d415cd367e306d6024ea303330ffdcfe863
Martina Gehwolf <martina.gehwolf@sap.com>
2020-06-18 07:21:12 +0000

    working deploy and subscribe with managed-hana

---
+++ srv/src/main/java/my/bookshop/handlers/SubscriptionHandler.java
@@ -0,0 +1,24 @@
+package my.bookshop.handlers;
+
+import org.springframework.stereotype.Component;
+
+import com.sap.cds.services.handler.EventHandler;
+import com.sap.cds.services.handler.annotations.Before;
+import com.sap.cds.services.handler.annotations.ServiceName;
+import com.sap.cds.services.mt.MtSubscriptionService;
+import com.sap.cds.services.mt.MtUnsubscribeEventContext;
+
+/**
+ * Handler that implements subscription logic
+ */
+@Component
+@ServiceName(MtSubscriptionService.DEFAULT_NAME)
+public class SubscriptionHandler implements EventHandler {
+
+	@Before(event = MtSubscriptionService.EVENT_UNSUBSCRIBE)
+	public void beforeUnsubscribe(MtUnsubscribeEventContext context) {
+		// always delete the subscription
+		context.setDelete(true);
+	}
+
+}
