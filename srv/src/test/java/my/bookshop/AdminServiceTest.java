395148773b021da92290ffd5119c34e78794cdd0
Martina Gehwolf <martina.gehwolf@sap.com>
2020-07-10 09:51:04 +0000

    removed MockMvc from local API test

--- srv/src/test/java/my/bookshop/AdminServiceTest.java
+++ srv/src/test/java/my/bookshop/AdminServiceTest.java
@@ -11,12 +11,10 @@ import javax.annotation.Resource;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
 
 import com.sap.cds.Result;
 import com.sap.cds.ql.Insert;
@@ -35,9 +33,6 @@ public class AdminServiceTest {
 
 	@Resource(name = AdminService_.CDS_NAME)
 	private DraftService adminService;
-
-	@Autowired
-	private MockMvc mockMvc;
 
 	@Test
 	@WithMockUser(username = "admin")
