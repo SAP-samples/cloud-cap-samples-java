3c95ba2971313fbbf549a334fc0e74db8aa70ebb
David H. Lam <david.lam@sap.com>
2021-08-27 16:39:32 +0200

    Tried to integrate correlate middleware

--- mtx-sidecar/server.js
+++ mtx-sidecar/server.js
@@ -17,8 +17,28 @@ const main = async () => {
 			return req.reject(e);
 		}
 	});
+
+	app.use(defaults.correlate)
 			
 	app.listen(PORT);
+}
+
+const defaults = {
+	// REVISIT: we need to align that with lib/req/context.js#L68 and remove redundancies
+	// ensure correlation id and set intermediate or augment cds.context until tx opened in protocol adpater
+	get correlate() {
+		return (req, _, next) => {
+			if (!cds.context) cds.context = {}
+			const id = cds.context.id
+				|| req.headers['x-correlation-id'] || req.headers['x-correlationid']
+				|| req.headers['x-request-id'] || req.headers['x-vcap-request-id']
+				|| cds.utils.uuid()
+			cds.context.id = req.headers['x-correlation-id'] = id
+			if (!cds.context._) cds.context._ = {}
+			if (!cds.context._.req) cds.context._.req = req
+			next()
+		}
+	}
 }
 
 main();
