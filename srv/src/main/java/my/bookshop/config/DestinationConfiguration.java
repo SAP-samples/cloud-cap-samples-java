7167174fad80fa40902b252b1eb495d3104f23a0
Marc Becker <marc.becker@sap.com>
2021-11-04 10:26:50 +0100

    Update to CDS Services 1.20.0 (#121)
    Showcase declarative destination configuration for API Hub destination

--- srv/src/main/java/my/bookshop/config/DestinationConfiguration.java
+++ srv/src/main/java/my/bookshop/config/DestinationConfiguration.java
@@ -1,42 +1,20 @@
-package my.bookshop.handlers;
+package my.bookshop.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
-import com.sap.cds.services.application.ApplicationLifecycleService;
-import com.sap.cds.services.handler.EventHandler;
-import com.sap.cds.services.handler.annotations.Before;
-import com.sap.cds.services.handler.annotations.ServiceName;
 import com.sap.cloud.sdk.cloudplatform.connectivity.DefaultDestinationLoader;
 import com.sap.cloud.sdk.cloudplatform.connectivity.DefaultHttpDestination;
 import com.sap.cloud.sdk.cloudplatform.connectivity.DestinationAccessor;
 
 @Component
-@ServiceName(ApplicationLifecycleService.DEFAULT_NAME)
-public class DestinationConfiguration implements EventHandler {
-
-	@Value("${api-hub.api-key:}")
-	private String apiKey;
+public class DestinationConfiguration {
 
 	@Autowired
 	private Environment environment;
-
-	@Before(event = ApplicationLifecycleService.EVENT_APPLICATION_PREPARED)
-	void initializeDestinations() {
-		if(apiKey != null && !apiKey.isEmpty()) {
-			DefaultHttpDestination httpDestination = DefaultHttpDestination
-					.builder("https://sandbox.api.sap.com/s4hanacloud")
-					.header("APIKey", apiKey)
-					.name("s4-business-partner-api").build();
-
-			DestinationAccessor.prependDestinationLoader(
-				new DefaultDestinationLoader().registerDestination(httpDestination));
-		}
-	}
 
 	@EventListener
 	void applicationReady(ApplicationReadyEvent ready) {
