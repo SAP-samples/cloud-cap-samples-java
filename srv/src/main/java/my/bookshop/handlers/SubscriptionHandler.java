6e783a2ed44a20f059a98dd9f57dc72d37a8e00d
Markus Ofterdinger <markus.ofterdinger@sap.com>
2022-04-04 13:28:10 +0200

    send audit log message to provider tenant (#140)

--- srv/src/main/java/my/bookshop/handlers/SubscriptionHandler.java
+++ srv/src/main/java/my/bookshop/handlers/SubscriptionHandler.java
@@ -1,10 +1,15 @@
 package my.bookshop.handlers;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
+import com.sap.cds.services.auditlog.AuditLogService;
 import com.sap.cds.services.handler.EventHandler;
+import com.sap.cds.services.handler.annotations.After;
 import com.sap.cds.services.handler.annotations.Before;
 import com.sap.cds.services.handler.annotations.ServiceName;
+import com.sap.cds.services.mt.MtSubscribeEventContext;
 import com.sap.cds.services.mt.MtSubscriptionService;
 import com.sap.cds.services.mt.MtUnsubscribeEventContext;
 
@@ -16,10 +21,21 @@ import com.sap.cds.services.mt.MtUnsubscribeEventContext;
 @ServiceName(MtSubscriptionService.DEFAULT_NAME)
 class SubscriptionHandler implements EventHandler {
 
+	@Autowired
+	private AuditLogService auditLog;
+
 	@Before(event = MtSubscriptionService.EVENT_UNSUBSCRIBE)
 	public void beforeUnsubscribe(MtUnsubscribeEventContext context) {
 		// always delete the subscription
 		context.setDelete(true);
+	}
+
+	@After(event = MtSubscriptionService.EVENT_SUBSCRIBE)
+	public void afterSubscribe(MtSubscribeEventContext context) {
+		String msg = String.format("New tenant '%s' subscribed.", context.getTenantId());
+
+		// send audit log security message to provider tenant as user's tenant is null
+		auditLog.logSecurityEvent("tenant subscribed", msg);
 	}
 
 }
