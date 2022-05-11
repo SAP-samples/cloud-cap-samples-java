8bd9b0fb2a8cfbc4078ec4d42320c719e9053d0d
BraunMatthias <59841349+BraunMatthias@users.noreply.github.com>
2022-05-11 11:44:15 +0200

    updated dependency versions (#141)

--- mtx-sidecar/server.js
+++ mtx-sidecar/server.js
@@ -8,17 +8,6 @@ const main = async () => {
 	await cds.connect.to('db');
 	const PORT = process.env.PORT || 4004;
 	await cds.mtx.in(app);
-	
-	const provisioning = await cds.connect.to('ProvisioningService');
-	provisioning.before(['UPDATE', 'DELETE', 'READ'], 'tenant', async (req) => {
-		// check for the scope "mtcallback"
-		if (!req.user.is('mtcallback')) {
-			// Reject request
-			const e = new Error('Forbidden');
-			e.code = 403;
-			return req.reject(e);
-		}
-	});
 
 	app.listen(PORT);
 }
