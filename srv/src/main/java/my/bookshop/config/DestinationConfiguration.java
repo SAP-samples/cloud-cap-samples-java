555d13d0b8f8426cc5611889cc0df4ef731935ca
Thomas Bonk <thomas.bonk@sap.com>
2023-08-10 08:55:25 +0200

    dynamically building URL for mocked API_BUSINESS_PARTNER

--- srv/src/main/java/my/bookshop/config/DestinationConfiguration.java
+++ srv/src/main/java/my/bookshop/config/DestinationConfiguration.java
@@ -1,5 +1,7 @@
 package my.bookshop.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.annotation.Profile;
@@ -7,22 +9,34 @@ import org.springframework.context.event.EventListener;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
+import com.sap.cds.services.runtime.CdsRuntime;
 import com.sap.cloud.sdk.cloudplatform.connectivity.DefaultDestinationLoader;
 import com.sap.cloud.sdk.cloudplatform.connectivity.DefaultHttpDestination;
 import com.sap.cloud.sdk.cloudplatform.connectivity.DestinationAccessor;
 import com.sap.cloud.sdk.cloudplatform.security.BasicCredentials;
 
+import my.bookshop.handlers.external.ApiBusinessPartnerEventMockHandler;
+
 @Component
 @Profile({"mocked", "mocked-api-business-partner"})
 public class DestinationConfiguration {
 
+	private final static Logger logger = LoggerFactory.getLogger(DestinationConfiguration.class);
+
 	@Autowired
 	private Environment environment;
+
+	@Autowired
+	private CdsRuntime runtime;
 
 	@EventListener
 	void applicationReady(ApplicationReadyEvent ready) {
 		Integer port = environment.getProperty("local.server.port", Integer.class);
+		String applicationUrl = runtime.getEnvironment().getApplicationInfo().getUrl();
 		String destinationName = environment.getProperty("cds.remote.services.'[API_BUSINESS_PARTNER]'.destination.name");
+
+		logger.info("Application URL: {}", applicationUrl);
+
 		if(port != null && destinationName != null) {
 			DefaultHttpDestination httpDestination = DefaultHttpDestination
 			.builder("http://localhost:" + port)
