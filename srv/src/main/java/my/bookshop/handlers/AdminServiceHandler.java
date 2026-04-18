7dbcb24c7d4249a9073e5ac648932f148b4ad688
Stefan Henke <stefan.henke@sap.com>
2026-04-18 17:01:48 +0200

    Revert "remove attachments code"
    This reverts commit 977e2462827cba37dd0f6203f68a8e0a2088253c.

--- srv/src/main/java/my/bookshop/handlers/AdminServiceHandler.java
+++ srv/src/main/java/my/bookshop/handlers/AdminServiceHandler.java
@@ -7,6 +7,7 @@ import cds.gen.adminservice.AdminService;
 import cds.gen.adminservice.AdminService_;
 import cds.gen.adminservice.Books;
 import cds.gen.adminservice.BooksAddToOrderContext;
+import cds.gen.adminservice.BooksCovers;
 import cds.gen.adminservice.Books_;
 import cds.gen.adminservice.OrderItems;
 import cds.gen.adminservice.OrderItems_;
@@ -18,6 +19,7 @@ import com.sap.cds.ql.Select;
 import com.sap.cds.ql.Update;
 import com.sap.cds.ql.Upsert;
 import com.sap.cds.ql.cqn.CqnAnalyzer;
+import com.sap.cds.ql.cqn.CqnStructuredTypeRef;
 import com.sap.cds.reflect.CdsModel;
 import com.sap.cds.services.ErrorStatuses;
 import com.sap.cds.services.EventContext;
@@ -26,6 +28,7 @@ import com.sap.cds.services.cds.CdsUpdateEventContext;
 import com.sap.cds.services.cds.CqnService;
 import com.sap.cds.services.draft.DraftCancelEventContext;
 import com.sap.cds.services.draft.DraftPatchEventContext;
+import com.sap.cds.services.draft.DraftService;
 import com.sap.cds.services.handler.EventHandler;
 import com.sap.cds.services.handler.annotations.Before;
 import com.sap.cds.services.handler.annotations.On;
@@ -333,6 +336,18 @@ class AdminServiceHandler implements EventHandler {
       }
     }
     return Arrays.asList(upload);
+  }
+
+  @Before(
+      event = {
+        CqnService.EVENT_CREATE,
+        CqnService.EVENT_UPDATE,
+        DraftService.EVENT_DRAFT_NEW,
+        DraftService.EVENT_DRAFT_PATCH
+      })
+  public void restoreCoversUpId(CqnStructuredTypeRef ref, BooksCovers cover) {
+    // restore up__ID, which is not provided via OData due to containment
+    cover.setUpId((String) analyzer.analyze(ref).rootKeys().get(Books.ID));
   }
 
   private BigDecimal defaultZero(BigDecimal decimal) {
