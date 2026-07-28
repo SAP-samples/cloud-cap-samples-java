13dda96398328faff9e57d3746d5f7b38a3fd258
Adrian Görler <adrian.goerler@sap.com>
2026-07-28 16:19:11 +0200

    Update UserDescriptionHandler.java

--- srv/src/main/java/my/bookshop/handlers/UserDescriptionHandler.java
+++ srv/src/main/java/my/bookshop/handlers/UserDescriptionHandler.java
@@ -3,29 +3,34 @@ package my.bookshop.handlers;
 import com.sap.cds.CdsData;
 import com.sap.cds.CdsDataProcessor;
 import com.sap.cds.services.draft.DraftCreateEventContext;
+import com.sap.cds.services.draft.DraftService;
 import com.sap.cds.services.handler.EventHandler;
-import com.sap.cds.services.handler.annotations.On;
+import com.sap.cds.services.handler.annotations.Before;
+import com.sap.cds.services.handler.annotations.ServiceName;
 import com.sap.cds.services.request.UserInfo;
 import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
+@ServiceName(value = "*", type = DraftService.class)
 public class UserDescriptionHandler implements EventHandler {
 
-  @On(service = "*")
+  @Autowired private UserInfo userInfo;
+
+  @Before
   protected void addDraftFields(DraftCreateEventContext context, List<CdsData> entries) {
     CdsDataProcessor.Filter filter =
-        (path, element, type) ->
-            switch (element.getName()) {
-              case "InProcessByUserDescription" -> true;
-              default -> false;
-            };
-
-    UserInfo userInfo = context.getUserInfo();
-    CdsDataProcessor.Generator generator =
-        (path, element, isNull) -> userInfo.getAdditionalAttribute("nickname");
+        (path, element, type) -> "InProcessByUserDescription".equals(element.getName());
+    CdsDataProcessor.Generator generator = (path, element, isNull) -> description();
     CdsDataProcessor dataProcessor = CdsDataProcessor.create().addGenerator(filter, generator);
 
     dataProcessor.process(entries, context.getTarget());
+  }
+
+  private String description() {
+    return userInfo.getAdditionalAttribute("given_name")
+        + " "
+        + userInfo.getAdditionalAttribute("family_name");
   }
 }
