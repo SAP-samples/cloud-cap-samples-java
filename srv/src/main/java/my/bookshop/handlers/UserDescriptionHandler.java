b01c379fcb2349bb0feb238e30ae605744e1f7c1
Adrian Görler <adrian.goerler@sap.com>
2026-07-29 11:23:48 +0200

    formatting

--- srv/src/main/java/my/bookshop/handlers/UserDescriptionHandler.java
+++ srv/src/main/java/my/bookshop/handlers/UserDescriptionHandler.java
@@ -23,9 +23,7 @@ public class UserDescriptionHandler implements EventHandler {
     CdsDataProcessor.Filter filter =
         (path, element, type) -> "InProcessByUserDescription".equals(element.getName());
     CdsDataProcessor.Generator generator = (path, element, isNull) -> description();
-    CdsDataProcessor.create()
-        .addGenerator(filter, generator)
-        .process(entries, context.getTarget());
+    CdsDataProcessor.create().addGenerator(filter, generator).process(entries, context.getTarget());
   }
 
   private String description() {
