0695cc7404e34421136c8c8dbd681a3ef409456c
Markus Ofterdinger <markus.ofterdinger@sap.com>
2026-09-16 11:12:49 +0200

    Postgres: Add spring-boot-starter-webmvc-test dependency and update import path in SimpleIntegrationTest

--- srv/src/test/java/my/bookshop/SimpleIntegrationTest.java
+++ srv/src/test/java/my/bookshop/SimpleIntegrationTest.java
@@ -3,7 +3,7 @@ package my.bookshop;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
