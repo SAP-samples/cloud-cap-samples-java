fbe6452bdb5ef139156d146485224f9a76b81478
Markus Ofterdinger <markus.ofterdinger@sap.com>
2026-09-16 13:53:49 +0200

    Refactor POM files and improve code formatting across the project

--- srv/src/test/java/my/bookshop/config/TestApplication.java
+++ srv/src/test/java/my/bookshop/config/TestApplication.java
@@ -1,15 +1,14 @@
 package my.bookshop.config;
 
+import my.bookshop.Application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import my.bookshop.Application;
 
 // Use this Application instead of the main one to run the app from the IDE
 
 @SpringBootApplication
 public class TestApplication {
-	public static void main(String[] args) {
-		SpringApplication.from(Application::main).run(args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.from(Application::main).run(args);
+  }
 }
